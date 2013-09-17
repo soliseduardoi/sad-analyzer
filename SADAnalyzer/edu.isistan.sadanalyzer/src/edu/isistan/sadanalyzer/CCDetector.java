567fcda6d7c6fa182b185e82eb4ad8e27e7ad6ce
soliseduardoi@gmail.com <soliseduardoi@gmail.com@a2f9a491-9b53-825b-51b9-b77a7c2dc482>
2013-09-17 22:35:56 +0000

    SADAnalyzer - SadAnalyzerEditor. Se agrega logica para realizar querys al modelo de uima

---
+++ SADAnalyzer/edu.isistan.sadanalyzer/src/edu/isistan/sadanalyzer/CCDetector.java
@@ -0,0 +1,17 @@
+package edu.isistan.sadanalyzer;
+
+public class CCDetector {
+	
+	public static String getRuleSetPath() {
+		return getModelsPath() + "rules/ruleset.xmi";
+	}
+	
+	private static String getModelsPath() {
+		String modelsPath = null;
+		if(modelsPath == null || modelsPath.isEmpty())
+			modelsPath = System.getenv("MODELS_PATH");
+		if(modelsPath == null || modelsPath.isEmpty())
+			modelsPath = System.getProperty("MODELS_PATH");
+		return modelsPath;
+	}
+}
